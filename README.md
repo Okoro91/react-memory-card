1d3c51584aef6f361e5d06a8c4b5032bef83d8fb
okoro <mi.fracri91@gmail.com>
2026-05-16 18:25:53 +0100

    add favicon and utility helpers for card shuffling and localStorage

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 A memory card game built with React that tests your recall ability. Click unique cards to earn points, but clicking the same card twice ends your streak! Features real-time score tracking, best score persistence, and dynamic card shuffling. Fetches character images from an external API.
+
+### Folder Structure
+
+src/
+├── components/
+│ ├── Card/
+│ │ ├── Card.jsx
+│ │ └── Card.module.css
+│ ├── Scoreboard/
+│ │ ├── Scoreboard.jsx
+│ │ └── Scoreboard.module.css
+│ ├── GameBoard/
+│ │ ├── GameBoard.jsx
+│ │ └── GameBoard.module.css
+│ └── Header/
+│ ├── Header.jsx
+│ └── Header.module.css
+├── hooks/
+│ ├── useGameLogic.js
+│ └── useApiFetch.js
+├── utils/
+│ ├── shuffleArray.js
+│ └── localStorage.js
+├── App.jsx
+├── App.module.css
+└── main.jsx
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,67 @@
 import React from "react";
-import Card from "./Card";
+import { shuffleArray } from "./utils/shuffleArray";
+import { saveToLocalStorage, getFromLocalStorage } from "./utils/localStorage";
+
+const cardsList = ["🐶", "🐱", "🐸", "🐵", "🦊"];
 
 const App = () => {
-  return <Card />;
+  const [cards, setCards] = React.useState(cardsList);
+  const [name, setName] = React.useState("");
+  const [getUser, setGetUser] = React.useState(getFromLocalStorage("user", ""));
+  const [saveUser, getSaveUser] = React.useState(
+    saveToLocalStorage("user", name),
+  );
+
+  const handleSave = () => {
+    getSaveUser(saveToLocalStorage("user", name));
+    setGetUser(getFromLocalStorage("user", ""));
+    alert("Name saved to local storage!");
+  };
+
+  const handleCardClick = () => {
+    setCards((prevCards) => shuffleArray(prevCards));
+  };
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold mb-4">localStorage Test</h1>
+      <input
+        type="text"
+        placeholder="Enter your name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        className="p-2 border border-gray-300 rounded mb-4"
+      />
+      <button
+        className="p-2 bg-blue-600 text-white rounded"
+        onClick={handleSave}
+      >
+        Save Name
+      </button>
+      <p>Saved Name: {getUser || "No name saved yet."}</p>
+      <p>Saved Name: {name}</p>
+
+      <h1>Memory Card Game</h1>
+      <div>
+        {cards.map((card, index) => (
+          <span
+            onClick={() => handleCardClick()}
+            key={index}
+            className="text-4xl m-4 cursor-pointer transition-transform duration-300 hover:scale-110
+          "
+          >
+            {card}
+          </span>
+        ))}
+      </div>
+      <button
+        className="p-2 bg-red-600 m-6 text-white rounded"
+        onClick={() => handleCardClick()}
+      >
+        Shuffle Cards
+      </button>
+    </>
+  );
 };
 
 export default App;
